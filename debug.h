3117a28814f1eccd344445fffb5c453813d3bd41
RockyLim92 <lrocky1229@gmail.com>
2019-05-17 21:07:38 +0900

    add debug header

---
+++ debug.h
@@ -0,0 +1,12 @@
+#ifndef _DEBUG_H_
+#define _DEBUG_H_
+
+#define DBG_ENABLE 1
+
+#ifdef DBG_ENABLE
+#define DBG_P(fmt, args...) printf("[DBG_P][%s:%s:%d] " fmt, __FILE__ ,__func__, __LINE__,##args)
+#else
+#define DBG_P(fmt, args...)
+#endif
+
+#endif
