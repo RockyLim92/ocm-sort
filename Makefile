9ca0b3c2c16961bf5fd52ab3c1c3423631b791a5
RockyLim92 <lrocky1229@gmail.com>
2019-06-04 18:02:39 +0900

    (merge + runformation) combined

--- Makefile
+++ Makefile
@@ -1,17 +1,18 @@
-CXX             = g++ -g
+CXX             = g++ -std=c++11
 SRCS            = $(wildcard *.cpp) $(wildcard *.cc)
 OBJS            = $(SRCS:.cpp=.o)
 TARGET          = ocmsort
 LIBS			= -lpthread -ltbb
 
 all : $(TARGET)
-	$(CXX) -std=c++11 -o $(TARGET) $(OBJS) $(INC) $(LIB_DIRS) $(LIBS)
+	$(CXX) -o $(TARGET) $(OBJS) $(INC) $(LIB_DIRS) $(LIBS)
 
 $(TARGET) :
-	$(CXX) -std=c++11 -c $(SRCS) $(INC) $(LIB_DIRS) $(LIBS)
+	$(CXX) -c $(SRCS) $(INC) $(LIB_DIRS) $(LIBS)
 
 clean :
 	rm -f $(TARGET)
 	rm -f *.o
+	
 
 
--- run_formation.cc
+++ run_formation.cc
@@ -1,7 +1,6 @@
 #define _LARGEFILE63_SOURCE
 
 #include <iostream>
-#include <sstream>	// kh
 #include <fstream>
 #include <cstdio>
 #include <sys/types.h>
@@ -14,40 +13,48 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <queue>	// kh
-#include "tbb/parallel_sort.h"	// kh
-#include "tbb/task_scheduler_init.h"	// kh
+#include <pthread.h>
+#include <queue>	//kh
+#include "tbb/parallel_sort.h"
+#include "tbb/task_scheduler_init.h"
+#include <atomic>
+
 #include "profile.h"
 #include "debug.h"
 
 using namespace std;
 
-#define DATA_SIZE (128)
-#define MEM_SIZE ((int64_t)2*1024*1024*1024) // 2GB
-#define NR_RUNS 8
-#define INPUT_PATH "./input.txt"
-#define OUTPUT_PATH "./output.txt"	// kh
-#define RUNS_DIR_PATH "./runs/"
-#define USE_EXISTING_DATA true
+#define INPUT_PATH "/mnt/input/input.txt"
+#define OUTPUT_PATH "/mnt/output/output.txt"
+#define RUNS_DIR_PATH "/mnt/output/runs/"
 
-#define BLK_SIZE (MEM_SIZE/NR_RUNS)	// kh
-#define BLK_PER_RUN (MEM_SIZE/BLK_SIZE)	// kh
-#define NR_ENTRIES_BLK (BLK_SIZE/DATA_SIZE) //	kh
-#define NR_ENTRIES_MEM (MEM_SIZE/DATA_SIZE)
-#define NR_ENTRIES (NR_ENTRIES_MEM*NR_RUNS)
-#define TOTAL_DATA_SIZE	DATA_SIZE*NR_ENTRIES
+#define TOTAL_DATA_SIZE ((int64_t)8*1024*1024*1024) // (8GB)
+#define MEM_SIZE ((int64_t)1*1024*1024*1024)	// (1GB)
+#define DATA_SIZE (128)
+#define NR_THREAD 4 // same as the number of buffer
+#define NR_THREAD_SORT 1
+#define BUFFER_SIZE (MEM_SIZE/NR_THREAD) // each mem buffer // (256MB)
+#define USE_EXISTING_DATA false
+#define USE_EXISTING_RUNS false	// kh
+#define IS_PARALLEL_SORT false
+
+#define NR_RUNS ((TOTAL_DATA_SIZE/MEM_SIZE)*NR_THREAD)	// kh	// (32-runs)
+#define BLK_SIZE (MEM_SIZE/NR_RUNS)	// kh	// (32MB)
+#define BLK_PER_RUN (BUFFER_SIZE/BLK_SIZE)	// kh	// (8-blks)
+#define NR_ENTRIES_BLK (BLK_SIZE/DATA_SIZE)	// kh	// (256-entries)
+#define NR_ENTRIES_BUFFER (BUFFER_SIZE / DATA_SIZE)
+#define NR_ENTRIES (TOTAL_DATA_SIZE / DATA_SIZE)
 
 int64_t WriteData(int fd, char *buf, int64_t buf_size);
 int64_t ReadData(int fd, char *buf, int64_t buf_size);
-
-
+void* t_RunFormation(void *data);
 
 struct Data{
 	uint32_t key;
 	char value[DATA_SIZE-sizeof(uint32_t)];
 };
 
-struct idx_Data{
+struct idx_Data{	// kh
 	uint32_t idx;
 	struct Data data;
 	bool operator<(const idx_Data &cmp) const {
@@ -60,17 +67,26 @@ struct idx_Data{
 
 
 Data *g_buffer;
-Data *blk_buffer;	// kh
-Data *merge_buffer;	// kh
+Data *blk_buffer;       // kh
+Data *merge_buffer;     // kh
+
+atomic_int run_idx;
+
+struct RunformationArgs{
+	int fd_input;
+	off_t st_offset;
+	char * r_buffer;
+	int64_t nbyte_load;
+};
 
 bool compare(struct Data a, struct Data b){
 	return  (a.key < b.key);
 }
 
-
-bool idx_compare(struct idx_Data a, struct idx_Data b){	// 	kh
+bool idx_compare(struct idx_Data a, struct idx_Data b){
 	return  (a.data.key < b.data.key);
 }
+
 
 void *randstring(size_t length, char *buf) {
 	static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
@@ -91,7 +107,9 @@ void *randstring(size_t length, char *buf) {
 int GenerateDataFile(){
 
 	srand((unsigned int)time(0));
+
 	int fd = open( INPUT_PATH, O_DIRECT | O_RDWR | O_CREAT | O_TRUNC | O_LARGEFILE, 0644);
+	//int fd = open( INPUT_PATH, O_DIRECT | O_RDWR | O_CREAT | 0644);
 	if(fd == -1){
 		fprintf(stderr, "FAIL: open file(%s) - %s\n", INPUT_PATH, strerror(errno));
 	}
@@ -114,6 +132,7 @@ int GenerateDataFile(){
 			DBG_P("nbyte_buffer: %ld\n", nbyte_buffer);
 
 			//char *buffer__ = (char *)g_buffer;
+
 			int64_t tmp_nbyte_written = WriteData(fd, (char *)g_buffer, nbyte_buffer);
 			if(tmp_nbyte_written == -1){
 				fprintf(stderr, "FAIL: write - %s\n", strerror(errno));
@@ -137,29 +156,68 @@ int GenerateDataFile(){
 	close(fd);
 }
 
-void RunFormation(){
+int RunFormation(){
+	
+	run_idx = 0;
 
-	int fd_input = open( INPUT_PATH, O_RDONLY);
+	pthread_t p_thread[NR_THREAD];
+	int thread_id[NR_THREAD];
+	struct RunformationArgs runformation_args[NR_THREAD];
+
+	int fd_input = open( INPUT_PATH, O_RDONLY | O_DIRECT);
 	if(fd_input == -1){
 		fprintf(stderr, "FAIL: open file - %s\n", strerror(errno));
 	}
+	
+	off_t cumulative_offset = 0;
+	for(int i=0; i<NR_THREAD; i++){
+		runformation_args[i].fd_input = fd_input;
+		runformation_args[i].st_offset = cumulative_offset;
+		runformation_args[i].r_buffer = (char *)g_buffer + cumulative_offset;
+		runformation_args[i].nbyte_load = TOTAL_DATA_SIZE / NR_THREAD;
+	
+
+		thread_id[i] = pthread_create(&p_thread[i], NULL, t_RunFormation, (void*)&runformation_args[i]);
+	
+		cumulative_offset += BUFFER_SIZE;
+	}
+	
+	int is_ok = 0;
+	int res = 0;
+	for(int i=0; i<NR_THREAD; i++){
+		pthread_join(p_thread[i], (void **)&is_ok);
+		if(is_ok != 1){
+			res = -1;
+		}
+	}
+
+	close(fd_input);
+	return res;
+}
+
+void* t_RunFormation(void *data){
+
+	struct RunformationArgs args = *(struct RunformationArgs*)data;
+	int fd_input = args.fd_input;
+	char *r_buffer = args.r_buffer;
+	Data *g_buffer__ = (Data *)r_buffer;
+	int64_t nbyte_load = args.nbyte_load;
 
 	int64_t nbyte_read = 0;
-	int run_idx=0;
 
-	while(nbyte_read < TOTAL_DATA_SIZE){
+	while(nbyte_read < nbyte_load){
 		
-
+		/* === PROFILE START (load)=== */
         struct timespec local_time_load[2];
         clock_gettime(CLOCK_MONOTONIC, &local_time_load[0]);
-        // load
-        int64_t tmp_byte_read = ReadData(fd_input, (char *)g_buffer, MEM_SIZE);
+        int64_t tmp_byte_read = ReadData(fd_input, r_buffer, BUFFER_SIZE);
         clock_gettime(CLOCK_MONOTONIC, &local_time_load[1]);
         calclock(local_time_load, &total_run_formation_load_time, &total_run_formation_load_count);
-        
-        
+		/* === PROFILE END === */
+
         if(tmp_byte_read == -1){
 			fprintf(stderr, "FAIL: read - %s\n", strerror(errno));
+			return ((void *)-1);
 		}
 		DBG_P("tmp_byte_read is: %zu\n", tmp_byte_read);
 
@@ -167,16 +225,19 @@ void RunFormation(){
 		DBG_P("nbyte_read: %ld\n", nbyte_read);
 
 		
+		/* === PROFILE START (sort) === */
         struct timespec local_time_sort[2];
         clock_gettime(CLOCK_MONOTONIC, &local_time_sort[0]);
-        /*q_sort*/
-	//	sort(&g_buffer[0], &g_buffer[0] + NR_ENTRIES_MEM, compare);
-        	tbb::parallel_sort(g_buffer, g_buffer + NR_ENTRIES_MEM, compare);
-	clock_gettime(CLOCK_MONOTONIC, &local_time_sort[1]);
+#if IS_PARALLEL_SORT
+		tbb::parallel_sort(&g_buffer__[0], &g_buffer__[0] + NR_ENTRIES_BUFFER, compare);
+#else
+		sort(&g_buffer__[0], &g_buffer__[0] + NR_ENTRIES_BUFFER, compare);
+#endif
+		clock_gettime(CLOCK_MONOTONIC, &local_time_sort[1]);
         calclock(local_time_sort, &total_run_formation_sort_time, &total_run_formation_sort_count);
-		
+		/* === PROFILE END === */
         
-        DBG_P("data sorted\n");
+		DBG_P("data sorted\n");
 
 		string run_path = RUNS_DIR_PATH;
 		run_path += "run_" + to_string(run_idx++);
@@ -184,21 +245,30 @@ void RunFormation(){
 		int fd_run = open( run_path.c_str(), O_DIRECT | O_RDWR | O_CREAT | O_LARGEFILE, 0644);
 		if(fd_run == -1){
 			fprintf(stderr, "FAIL: open file(%s) - %s\n", run_path.c_str() , strerror(errno));
+			return ((void *)-1);
 		}
 
+		/* === PROFILE START (flush) === */
         struct timespec local_time_store[2];
         clock_gettime(CLOCK_MONOTONIC, &local_time_store[0]);
-		// store
-        WriteData(fd_run, (char *)g_buffer, MEM_SIZE);
+        int64_t tmp_byte_write = WriteData(fd_run, (char *)g_buffer__, BUFFER_SIZE);
         clock_gettime(CLOCK_MONOTONIC, &local_time_store[1]);
         calclock(local_time_store, &total_run_formation_store_time, &total_run_formation_store_count);
+		/* === PROFILE END === */
 
-
-		DBG_P("run %d is flushed\n", run_idx-1);
+        if(tmp_byte_write == -1){
+			fprintf(stderr, "FAIL: write - %s\n", strerror(errno));
+			return ((void *)-1);
+		}
+		DBG_P("tmp_byte_write is: %zu\n", tmp_byte_read);
 		
+
+		DBG_P("run %d is done\n", run_idx-1);
 		close(fd_run);
 	}
-	close(fd_input);
+
+	DBG_P("thread is done\n");
+	return ((void *)1);
 }
 
 void Merge(){
@@ -213,7 +283,7 @@ void Merge(){
 	if(fd_output == -1){
 		fprintf(stderr, "FAIL: open file - %s\n", strerror(errno));
 	}
-	
+
 	for (int i = 0; i < run_count; i++){
 		string run_path = RUNS_DIR_PATH;
 		run_path += "run_" + to_string(i);
@@ -224,12 +294,12 @@ void Merge(){
 		}
 		ReadData(fd_run[i], (char*)(blk_buffer + i * NR_ENTRIES_BLK), BLK_SIZE);
 		ptr_run[i]++;
-		
+
 		idx_Data tmp_buffer;
 		tmp_buffer.data = blk_buffer[i * NR_ENTRIES_BLK];
 		tmp_buffer.idx = i;
 		pq.push(tmp_buffer);
-		
+
 		ptr_blk[i]++;
 	}
 
@@ -240,8 +310,8 @@ void Merge(){
 		merge_buffer[ptr_mrg] = pq.top().data;
 		pq.pop();
 
-		if (ptr_mrg == NR_ENTRIES_BLK){		// merge buffer is full
-			DBG_P("merged key: %u\n", merge_buffer[0].key);	// check key sorting
+		if (ptr_mrg == NR_ENTRIES_BLK){         // merge buffer is full
+			DBG_P("merged key: %u\n", merge_buffer[0].key); // check key sorting
 			int64_t tmp_nbyte_merged = WriteData(fd_output, (char *)merge_buffer, BLK_SIZE);
 			if(tmp_nbyte_merged == -1){
 				fprintf(stderr, "FAIL: write - %s\n", strerror(errno));
@@ -250,62 +320,62 @@ void Merge(){
 			DBG_P("nbyte_merged: %ld\n", nbyte_merged);
 		} else ptr_mrg++;
 
-		if ((ptr_blk[pop_idx] == NR_ENTRIES_BLK) && (ptr_run[pop_idx] < BLK_PER_RUN)){	// run and block is empty
+		if ((ptr_blk[pop_idx] == NR_ENTRIES_BLK) && (ptr_run[pop_idx] < BLK_PER_RUN)){  // run and block is empty
 			ReadData(fd_run[pop_idx], (char*)(blk_buffer + pop_idx*NR_ENTRIES_BLK), BLK_SIZE);
 			ptr_blk[pop_idx] = 0;
 			ptr_run[pop_idx]++;
 		}
-
 		idx_Data tmp_buffer;
 		tmp_buffer.data = blk_buffer[(pop_idx * NR_ENTRIES_BLK) + ptr_blk[pop_idx]];
 		tmp_buffer.idx = pop_idx;
 		ptr_blk[pop_idx]++;
 		pq.push(tmp_buffer);
 	}
-	
 	for (int i = 0; i < run_count; i++)
 		close(fd_run[i]);
 	close(fd_output);
-
-
-
-
 }
 
+
+
+
 void PrintStat(){
-    printf("runformation - total time: %llu, total_cout: %llu\n", total_run_formation_time, total_run_formation_count);
-    printf("runformation - load time: %llu, load_cout: %llu\n", total_run_formation_load_time, total_run_formation_load_count);
-    printf("runformation - sort time: %llu, sort_cout: %llu\n", total_run_formation_sort_time, total_run_formation_sort_count);
-    printf("runformation - store time: %llu, load_cout: %llu\n", total_run_formation_store_time, total_run_formation_store_count);
+	printf("run_formation;load;sort;store\n");
+	printf("%.2f;%.2f;%.2f;%.2f\n",(double)total_run_formation_time/1000000000,
+							  (double)total_run_formation_load_time/1000000000,
+							  (double)total_run_formation_sort_time/1000000000,
+							  (double)total_run_formation_store_time/1000000000);
 }
 
 void PrintConfig(){
-	printf("VALUESIZE: %d\n", DATA_SIZE);
-	printf("MEM_SIZE: %zu\n", MEM_SIZE);
-	printf("NR_RUNS: %d\n", NR_RUNS);
-	printf("NR_ENTRIES_MEM: %zu\n", NR_ENTRIES_MEM);
-	printf("NR_ENTRIES: %zu\n", NR_ENTRIES);
 	printf("TOTAL_DATA_SIZE %zu\n", TOTAL_DATA_SIZE);
-	printf("sizeof Data: %zu\n", sizeof(Data));
+	printf("DATA_SIZE: %d\n", DATA_SIZE);
+	printf("MEM_SIZE: %zu\n", MEM_SIZE);
+	printf("NR_THREAD: %d\n", NR_THREAD);
+	printf("NR_BUFFER: %d\n", NR_THREAD);
+	printf("BUFFER_SIZE: %zu\n", BUFFER_SIZE);
+	printf("NR_ENTRIES_BUFFER: %zu\n", NR_ENTRIES_BUFFER);
+	printf("NR_ENTRIES: %zu\n", NR_ENTRIES);
 }
 
 int main(int argc, char* argv[]){
 
-	tbb::task_scheduler_init init();	// kh
-	PrintConfig();
+	int res = 0;
 	
-	// unaligned memory allocation
-	// Data *tmp = new Data[NR_ENTRIES_MEM];
+	// init parallel sort nr_threads
+	tbb::task_scheduler_init init(NR_THREAD_SORT);
 	
 	// aligned memory allocation
 	void *mem;
-	void *mem2;
+	void *mem2;	// kh
 	posix_memalign(&mem, 4096, MEM_SIZE);
-	posix_memalign(&mem2, 4096, MEM_SIZE);	//	kh
+	posix_memalign(&mem2, 4096, MEM_SIZE);
 	Data *tmp = new (mem) Data;
-	Data *tmp2 = new (mem2) Data;	//	kh
+	Data *tmp2 = new (mem2) Data;
 	g_buffer = tmp;
-	merge_buffer = tmp2;	//	kh
+	merge_buffer = tmp2;	
+
+	PrintConfig();
 	
 #if !USE_EXISTING_DATA
 	if(GenerateDataFile() != -1){
@@ -313,20 +383,27 @@ int main(int argc, char* argv[]){
 	}
 #endif
 
+#if !USE_EXISTING_RUNS
+	/* === PROFILE START (run formation)=== */
 	struct timespec local_time1[2];
 	clock_gettime(CLOCK_MONOTONIC, &local_time1[0]);
-	/* run formation */
-//	RunFormation();
+	res = RunFormation();
 	clock_gettime(CLOCK_MONOTONIC, &local_time1[1]);
 	calclock(local_time1, &total_run_formation_time, &total_run_formation_count);
+	/* === PROFILE END === */
+	if(res != 0){
+		fprintf(stderr, "FAIL: run formation\n");
+		return -1;
+	}
+#endif
 
-	/* merge */	
+	/* merge */
 	struct timeval diff, startTV, endTV;
 	gettimeofday(&startTV, NULL);
-	Merge();	//	kh
+	Merge();        //      kh
 	gettimeofday(&endTV, NULL);
 	timersub(&endTV, &startTV, &diff);
-	printf("*time taken = %ld %ld \n", diff.tv_sec, diff.tv_usec);
+	printf("*time taken for merge = %ld %ld \n", diff.tv_sec, diff.tv_usec);
 	PrintStat();
 
 	free(tmp);
